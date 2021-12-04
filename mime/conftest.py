ee541cfb622ff894a7f9e56e351ee08735882c8a
Vince <vincivenv@gmail.com>
2021-12-04 17:46:25 +0300

    Here be mimes!

---
+++ mime/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from mime.users.models import User
+from mime.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
