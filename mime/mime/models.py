11137d168e8019a93d15d51012e84e1a344cba14
Vince <vincivenv@gmail.com>
2021-12-06 19:22:09 +0300

    refactor: split mime and location model

--- mime/mime/models.py
+++ mime/mime/models.py
@@ -6,13 +6,27 @@ name: str
 created at: dt
 no_dumps: int -> objects found in photo
 dumps_name: str -> single name currently since this is a classification model
-location of dumpsite -> [city and location]
+location of dumpsite -> [city and estate]
 """
 
 import uuid
 
 from django.conf import settings
 from django.db import models
+
+
+class Location(models.Model):
+    """Location model
+
+    Model used to store the mime location details
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    city = models.CharField(max_length=50)
+    estate = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.city + ", " + self.estate if self.estate else self.city
 
 
 class Mime(models.Model):
@@ -22,13 +36,10 @@ class Mime(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True
     )
-    name = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
     # Calling it dumps, can't figure out a name to group the classes to
-    no_dumps = models.IntegerField()
-    dumps_name = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    location = models.CharField(max_length=255)
+    no_dumps = models.PositiveIntegerField(default=1)
+    dumps_name = models.CharField(max_length=50)
 
     def __str__(self):
         """Class names include:
@@ -40,4 +51,4 @@ class Mime(models.Model):
 
         :return: name of the classified object
         """
-        return self.name
+        return self.dumps_name
