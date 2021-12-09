31d73fc7c39f7a6e596d2720b3b1f69f00c50dd0
Vince <vincivenv@gmail.com>
2021-12-10 01:22:03 +0300

    chore: some comments

--- mime/users/tests/test_drf_urls.py
+++ mime/users/tests/test_drf_urls.py
@@ -17,8 +17,3 @@ def test_user_detail(user: User):
 def test_user_list():
     assert reverse("api:user-list") == "/api/users/"
     assert resolve("/api/users/").view_name == "api:user-list"
-
-
-def test_user_me():
-    assert reverse("api:user-me") == "/api/users/me/"
-    assert resolve("/api/users/me/").view_name == "api:user-me"
